833d77ce3e799b7b75b1a4123098fe967eed36d3
piyushpal <piyushpal9532@gmail.com>
2025-05-13 12:22:06 +0530

    added cors

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 from app.extractor import hybrid_extract
 
 # Load city list
@@ -7,6 +8,14 @@ with open("data/citilist.txt") as f:
     cities = [line.strip() for line in f]
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Define request body schema
 class FlightRequest(BaseModel):
